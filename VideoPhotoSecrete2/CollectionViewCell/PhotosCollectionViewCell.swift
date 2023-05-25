117b3e6c2a0175f1157a979f88ee3b046ce6a319
Duc Nguyen <108277804+HornSeller@users.noreply.github.com>
2023-05-25 10:24:54 +0700

    add

--- VideoPhotoSecrete2/CollectionViewCell/PhotosCollectionViewCell.swift
+++ VideoPhotoSecrete2/CollectionViewCell/PhotosCollectionViewCell.swift
@@ -9,13 +9,14 @@ import UIKit
 
 class PhotosCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var checkBoxImgView: UIImageView!
     @IBOutlet weak var countLb: UILabel!
     @IBOutlet weak var titleLb: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        
+        checkBoxImgView.isHidden = true
     }
 
 }
