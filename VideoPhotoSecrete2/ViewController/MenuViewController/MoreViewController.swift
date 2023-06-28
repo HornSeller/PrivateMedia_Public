137a7399b282461a7c395f52f34f5a85339e0563
Duc Nguyen <108277804+HornSeller@users.noreply.github.com>
2023-06-28 08:17:38 +0700

    commit

--- VideoPhotoSecrete2/ViewController/MenuViewController/MoreViewController.swift
+++ VideoPhotoSecrete2/ViewController/MenuViewController/MoreViewController.swift
@@ -6,9 +6,11 @@
 //
 
 import UIKit
+import StoreKit
+import MessageUI
 import GoogleMobileAds
 
-class MoreViewController: UIViewController, GADBannerViewDelegate {
+class MoreViewController: UIViewController, GADBannerViewDelegate, MFMailComposeViewControllerDelegate {
 
     var bannerView: GADBannerView!
     
@@ -71,4 +73,59 @@ class MoreViewController: UIViewController, GADBannerViewDelegate {
     func bannerViewDidDismissScreen(_ bannerView: GADBannerView) {
       print("bannerViewDidDismissScreen")
     }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+
+        switch result {
+        case .cancelled:
+            // Xử lý khi người dùng hủy gửi email
+            break
+        case .saved:
+            // Xử lý khi email được lưu như phiên bản nháp
+            break
+        case .sent:
+            // Xử lý khi email được gửi thành công
+            break
+        case .failed:
+            // Xử lý khi gửi email thất bại
+            break
+        @unknown default:
+            break
+        }
+    }
+    
+    @IBAction func feedbackBtnTapped(_ sender: UIButton) {
+        if MFMailComposeViewController.canSendMail() {
+            let composeVC = MFMailComposeViewController()
+            composeVC.mailComposeDelegate = self
+            composeVC.setToRecipients(["youraddress@example.com"])
+            composeVC.setSubject("Feedback your app!")
+            composeVC.setMessageBody("Dear AppChannel team, \n\n", isHTML: false)
+            self.present(composeVC, animated: true, completion: nil)
+        }
+        else {
+            let alert = UIAlertController(title: "Error", message: "Please try again later", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true)
+        }
+    }
+    
+    @IBAction func rateBtnTapped(_ sender: UIButton) {
+        SKStoreReviewController.requestReview()
+    }
+    
+    @IBAction func shareBtnTapped(_ sender: UIButton) {
+        if let name = URL(string: "https://apps.apple.com/vn/app/asphalt-9-legends/id1491129197?mt=12"), !name.absoluteString.isEmpty {
+          let objectsToShare = [name]
+          let activityVC = UIActivityViewController(activityItems: objectsToShare, applicationActivities: nil)
+          self.present(activityVC, animated: true, completion: nil)
+        } else {
+          // show alert for not available
+            let alert = UIAlertController(title: "Error", message: "Please try again later", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true)
+        }
+    }
+    
 }
